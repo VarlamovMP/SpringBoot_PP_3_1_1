d16edafe7c49a2ebc53c18751859610d5d3e4aff
Михаил <koko0104199@gmail.com>
2024-11-24 01:33:36 +0300

    2

--- pre-project/databaseexam/src/main/resources/test.sql
+++
@@ -1,9 +0,0 @@
-insert into users(name, age, email) values('Goga', 2, 'phsams@mail.ru');
-#select * from users;
-#select * from users where id=1;
-#select * from users where id=2;
-#select name from users where id=2;
-#select name, age from users where id=2;
-#update users set name= 'Serg', age= 55 where id=1;
-#select * from users where name='Serg';
-#delete from users;
